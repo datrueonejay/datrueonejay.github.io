1c0b33806ca01b64d66f7b01105da6e87ebae9a5
datrueonejay <jayarquelada@gmail.com>
2017-07-06 17:04:50 -0400

    Added Google Play Store to bottom, added test nav bar to be used for carousel.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,7 +5,15 @@ $(document).ready(function() {
     "height": tallness
   });
   $("#footer").height((tallness/3));
-
+  $("#nav0").hover(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $("#nav1").hover(function(){
+    $("#myCarousel").carousel(1);
+  });
+  $("#nav2").hover(function(){
+    $("#myCarousel").carousel(2);
+  });
   // $(window).resize(function(){
   //   var tallness = $("#header").outerHeight(true);
   //   $(".filler").css({
