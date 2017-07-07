d90afa40179d277acc4160fab1f9391f0fb6dbf8
datrueonejay <jayarquelada@gmail.com>
2017-07-07 00:36:57 -0400

    Working on changing backgrounds and switchings through pages on index.html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,14 +5,47 @@ $(document).ready(function() {
     "height": tallness
   });
   $("#footer").height((tallness/3));
-  $("#nav0").hover(function(){
+  $("#footerDivideContainer").height((tallness/3));
+
+  $("#nav0").click(function(){
     $("#myCarousel").carousel(0);
+    $(".navPage").removeClass("active");
+    $(this).addClass("active");
+    $("#bodyContainer").css({
+      "background-color": "#FF9A45"
+    });
+    $("body").css({
+      "background-color": "#FF9A45"
+    });
+    $(this).css({
+      "background-color": "#FF9A45"
+    });
+    // $("#bodyContainer").animate({backgroundColor: "red"});
   });
-  $("#nav1").hover(function(){
+
+
+
+  $("#nav1").click(function(){
     $("#myCarousel").carousel(1);
+    $(".navPage").removeClass("active");
+    $(this).addClass("active");
+    $("#bodyContainer").css({
+      "background-color": "#F76C6C"
+    });
+    $("body").css({
+      "background-color": "#F76C6C"
+    });
   });
-  $("#nav2").hover(function(){
+  $("#nav2").click(function(){
     $("#myCarousel").carousel(2);
+    $(".navPage").removeClass("active");
+    $(this).addClass("active");
+    $("#bodyContainer").css({
+      "background-color": "#FDFF77"
+    });
+    $("body").css({
+      "background-color": "#FDFF77"
+    });
   });
   // $(window).resize(function(){
   //   var tallness = $("#header").outerHeight(true);
