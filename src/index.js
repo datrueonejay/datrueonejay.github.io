711c46d723d30eb9ffba2977f7149585575a2b3b
Jayden <jayarquelada@gmail.com>
2019-02-08 16:57:16 -0500

    Resume update, and fixed pdf not being viewable on chrome

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './Components/App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
+// needed because of following issue with chrome
+// https://github.com/gatsbyjs/gatsby/issues/10899
+// fix https://stackoverflow.com/questions/49604821/cache-busting-with-create-react-app
+unregister();
