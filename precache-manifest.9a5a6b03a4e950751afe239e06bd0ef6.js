55561d5502b1fdf113e5b9b7b468f2c454527fad
datrueonejay <jayarquelada@gmail.com>
2019-09-01 15:16:12 -0400

    Updates

---
+++ precache-manifest.9a5a6b03a4e950751afe239e06bd0ef6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca46786acbb1137aa6197f556bccca81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8860c53f7549b631399c",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5435a5cb9b910b736277",
+    "url": "/static/css/main.a85d5f5c.chunk.css"
+  },
+  {
+    "revision": "8860c53f7549b631399c",
+    "url": "/static/js/2.efa7d920.chunk.js"
+  },
+  {
+    "revision": "5435a5cb9b910b736277",
+    "url": "/static/js/main.e1aaf64c.chunk.js"
+  },
+  {
+    "revision": "349a9cfdc5b2c28454dd",
+    "url": "/static/js/runtime~main.d12dfcf3.js"
+  },
+  {
+    "revision": "be1188731bba55ce20431a61a00f6dc6",
+    "url": "/static/media/JaydenArqueladaResume.be118873.pdf"
+  },
+  {
+    "revision": "de119c05e37a71d4f9f8d4af95ccbfe8",
+    "url": "/static/media/bankIcon.de119c05.png"
+  },
+  {
+    "revision": "854d1040388e9ec2a95f8aa52329ffe3",
+    "url": "/static/media/droidBackground.854d1040.jpg"
+  },
+  {
+    "revision": "cb8364239f09267d6a7f699a6fcc73af",
+    "url": "/static/media/foodForThought.cb836423.png"
+  }
+]);
