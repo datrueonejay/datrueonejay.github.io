543f32cae70c83fbf01a7fc081580c4233ccce0f
Jayden Arquelada <jayarquelada@gmail.com>
2020-05-26 16:23:35 -0400

    Added youtube

--- src/Components/Dock.js
+++ src/Components/Dock.js
@@ -1,26 +1,55 @@
-import React, { Component } from 'react';
-import DockApp from './DockApp';
-import { OS } from '../Redux/Actions/actionConstants';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import DockApp from "./DockApp";
+import { OS } from "../Redux/Actions/actionConstants";
+import { connect } from "react-redux";
 
 // var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
 
 class Dock extends Component {
   render() {
-    return(
-      <div className='Dock' >
-        <DockApp icon={this.props.os === OS.DROID ? 
-          require('../images/github.png') :
-          require('../images/githubIOS.png')} link='https://github.com/datrueonejay'/>
-        <DockApp icon={this.props.os === OS.DROID ? 
-          require('../images/linkedIn.png') :
-          require('../images/linkedInIOS.png')} link='https://www.linkedin.com/in/jayquelada/'/>
-        <DockApp icon={this.props.os === OS.DROID ? 
-          require('../images/resume.png') :
-          require('../images/resumeIOS.png')} link={require('../assets/JaydenArqueladaResume.pdf')} />
-        <DockApp icon={this.props.os === OS.DROID ? 
-          require('../images/contact.png') :
-          require('../images/contactIOS.png')}  link="mailto:jayarquelada@gmail.com" target='_top'/>
+    return (
+      <div className="Dock">
+        <DockApp
+          icon={
+            this.props.os === OS.DROID
+              ? require("../images/youtube.png")
+              : require("../images/youtubeIOS.png")
+          }
+          link="https://www.youtube.com/channel/UCDAA0AZRunZ5-NY5H1np2pw"
+        />
+        <DockApp
+          icon={
+            this.props.os === OS.DROID
+              ? require("../images/github.png")
+              : require("../images/githubIOS.png")
+          }
+          link="https://github.com/datrueonejay"
+        />
+        <DockApp
+          icon={
+            this.props.os === OS.DROID
+              ? require("../images/linkedIn.png")
+              : require("../images/linkedInIOS.png")
+          }
+          link="https://www.linkedin.com/in/jayquelada/"
+        />
+        <DockApp
+          icon={
+            this.props.os === OS.DROID
+              ? require("../images/resume.png")
+              : require("../images/resumeIOS.png")
+          }
+          link={require("../assets/JaydenArqueladaResume.pdf")}
+        />
+        <DockApp
+          icon={
+            this.props.os === OS.DROID
+              ? require("../images/contact.png")
+              : require("../images/contactIOS.png")
+          }
+          link="mailto:jayarquelada@gmail.com"
+          target="_top"
+        />
       </div>
     );
   }
@@ -29,7 +58,7 @@ class Dock extends Component {
 function mapStateToProps(state) {
   return {
     os: state.os,
-  }
+  };
 }
 
 export default connect(mapStateToProps)(Dock);
